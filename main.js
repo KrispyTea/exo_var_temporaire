2a363180793c296afa1b42865bf68609de90b4e8
KrispyTea <80962605+KrispyTea@users.noreply.github.com>
2021-04-19 16:08:23 +0200

    over

---
+++ main.js
@@ -0,0 +1,59 @@
+let a = 1
+let b = 2
+let c = 3
+let temp 
+temp = a
+a = b
+b = c
+c = temp
+console.log(a,b,c)
+
+let ab = 1
+let bb = 2
+let cb = 3
+let db = 4
+let tempo
+tempo = ab
+ab = db
+db = tempo
+tempo = bb
+bb = cb
+cb = tempo
+console.log(ab,bb,cb,db);
+
+let ac = 1
+let bc = 2
+let cc = 3
+let dc = 4
+tempor = ac
+ac = bc
+bc = cc
+cc = dc
+dc = tempor
+console.log(ac,bc,cc,dc)
+
+let tableau = [1, 2, 3, 4]
+let temporar
+temporar = tableau[0]
+tableau[0] = tableau[3]
+tableau[3] = temporar
+temporar = tableau[1]
+tableau[1] = tableau[2]
+tableau[2] = temporar
+console.log(tableau);
+
+let ad = 1
+let bd = 2
+let cd = 3
+let dd = 4
+let ed = 5
+let fd = 6
+let tempora
+tempora = cd
+cd = ad
+ad = dd
+dd = bd
+bd = ed
+ed = fd
+fd = tempora
+console.log(ad,bd,cd,dd,ed,fd)
